d64520cb36f0509c99a70de8980c24bfea9729c1
Ishimwe JOSEPH WazaCode <wazacode@gmail.com>
2023-10-06 11:09:46 +0300

    Update my_enumerable.rb
    Def filter added!

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -8,4 +8,11 @@ module MyEnumerable
     each { |item| return true if yield(item) }
     false
   end
+
+  def filter
+    new_res = []
+    each { |item| new_res << item if yield(item) }
+    new_res.join(', ')
+  end
+  
 end
